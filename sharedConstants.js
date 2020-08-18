d7c404d697ed9abedd7d9686ddba45fe45ef5a92
Jan Mikusch <jmikusch.mmt-b2017@fh-salzburg.ac.at>
2020-08-18 16:32:53 +0200

    Update constants for ludecat server

--- sharedConstants.js
+++ sharedConstants.js
@@ -1,9 +1,11 @@
 // Define all constants usefull by the server and the client
 var constant = {
 
-  SERVER_PORT:              4242,
-  SOCKET_PORT:              1337,
-  SOCKET_ADDR:              'http://localhost',
+  SERVER_PORT:              10011,
+  SOCKET_PORT:              10012,
+  SOCKET_PORT_PUBLIC:       443,
+  SOCKET_ADDR:              'https://io-games.lude.cat',
+  SOCKET_PATH:              '/flappybird',
 
   SCREEN_WIDTH:             900,
   SCREEN_HEIGHT:            768,
@@ -19,7 +21,7 @@ var constant = {
   DISTANCE_BETWEEN_PIPES:   380,
   MIN_PIPE_HEIGHT:          60,
   MAX_PIPE_HEIGHT:          630,
-  HEIGHT_BETWEEN_PIPES:     150
+  HEIGHT_BETWEEN_PIPES:     200 // 150
 };
 
 // To be use by the server part, we have to provide the object with exports
